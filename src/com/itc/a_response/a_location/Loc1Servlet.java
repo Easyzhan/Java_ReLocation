1dbfd37eac399e3b26b33e784108e0280ba50227
VD-Dragon <1037445632@qq.com>
2017-07-31 23:01:27 +0800

    java_servelet 重定向

---
+++ src/com/itc/a_response/a_location/Loc1Servlet.java
@@ -0,0 +1,37 @@
+package com.itc.a_response.a_location;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 重定向
+ */
+@WebServlet("/loc1")
+public class Loc1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("跟我借钱?我没钱啊");
+		System.out.println("找你老妈去啊");
+		
+		response.sendRedirect("/KBNY/loc2");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
